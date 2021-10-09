14e850119af57ad4b5655c4dc319e0a5e98a16c6
komori-n <ikamat.kmr@gmail.com>
2021-10-09 15:02:28 +0900

    feat: add parser for let statements

---
+++ src/monkey/parser/parser.go
@@ -0,0 +1,101 @@
+package parser
+
+import (
+	"fmt"
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l      *lexer.Lexer
+	errors []string
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l, errors: []string{}}
+
+	// Read first two tokens (curToken and peekToken)
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+
+	for p.curToken.Kind != token.Eof {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Kind {
+	case token.Let:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.Ident) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.Assign) {
+		return nil
+	}
+
+	// TODO: Skip until semicolon without parsing expression and error
+	for !p.curTokenIs(token.Semicolon) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenKind) bool {
+	return p.curToken.Kind == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenKind) bool {
+	return p.peekToken.Kind == t
+}
+
+func (p *Parser) expectPeek(t token.TokenKind) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenKind) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead (L%d)", t, p.peekToken.Kind, p.peekToken.Line)
+	p.errors = append(p.errors, msg)
+}
